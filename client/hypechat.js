a8926c65f9d2e627262591280b9f5fa59ec00dcc
llmanuel <manuel.llauro@restorando.com>
2019-06-28 04:26:44 -0300

    getting masters info

--- client/hypechat.js
+++ client/hypechat.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default (token) => {
   return axios.create({
-    baseURL: 'https://app-server-t2.herokuapp.com',
+    baseURL: 'https://hypechat-t2.herokuapp.com',
     timeout: 5000,
     headers: { 'x-auth-token': token }
   });  
